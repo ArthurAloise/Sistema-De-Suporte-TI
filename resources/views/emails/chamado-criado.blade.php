dfdc595c6cea69eaf2d5ec942f6349232cd97819
Stanley-Maloney <stanley.filho@faculdadesapiens.edu.br>
2025-05-06 13:24:04 -0400

    mensagens do email

---
+++ resources/views/emails/chamado-criado.blade.php
@@ -0,0 +1,4 @@
+<h2>Chamado criado com sucesso!</h2>
+<p><strong>ID:</strong> {{ $chamado->id }}</p>
+<p><strong>Título:</strong> {{ $chamado->titulo }}</p>
+<p><strong>Descrição:</strong> {{ $chamado->descricao }}</p>
