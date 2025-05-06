df48305788802afb6fc1ca6f4351092c5771585c
Stanley-Maloney <stanley.filho@faculdadesapiens.edu.br>
2025-05-06 13:23:50 -0400

    mensagens do email

---
+++ resources/views/emails/chamado-resolvido.blade.php
@@ -0,0 +1,4 @@
+<h2>Chamado resolvido</h2>
+<p>O chamado #{{ $chamado->id }} foi resolvido.</p>
+<p><strong>Título:</strong> {{ $chamado->titulo }}</p>
+<p><strong>Descrição da resolução:</strong> {{ $chamado->descricao_resolucao }}</p>
---
+++ resources/views/emails/tecnico-alterado.blade.php
@@ -0,0 +1,4 @@
+<h2>Novo chamado atribuído</h2>
+<p>Você foi designado para o chamado #{{ $chamado->id }}.</p>
+<p><strong>Título:</strong> {{ $chamado->titulo }}</p>
+<p><strong>Prioridade:</strong> {{ $chamado->prioridade }}</p>
